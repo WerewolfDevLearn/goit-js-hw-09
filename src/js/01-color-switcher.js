e2a0e4d8f824f81a67f84cfde76bbe723be7181c
WerewolfDevLearn <werewolfdevlearn@gmail.com>
2023-02-10 18:17:40 +0200

    first task done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const btnStrt = document.querySelector('[data-start]');
+const btnStp = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+btnStp.setAttribute('disabled', '');
+let interval;
 
+btnStrt.addEventListener('click', startColorChange);
+btnStp.addEventListener('click', stopColorChange);
+
+function startColorChange() {
+  interval = setInterval(bodyColor, 1000);
+  btnStrt.setAttribute('disabled', '');
+  btnStp.removeAttribute('disabled');
+}
+function stopColorChange() {
+  clearInterval(interval);
+  btnStrt.removeAttribute('disabled');
+  btnStp.setAttribute('disabled', '');
+}
+
+function bodyColor() {
+  body.style.backgroundColor = getRandomHexColor();
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
