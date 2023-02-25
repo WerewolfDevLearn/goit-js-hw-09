915acf981e1c0aef8518e3a75923029d0cb5d7ca
WerewolfDevLearn <werewolfdevlearn@gmail.com>
2023-02-25 21:26:12 +0200

    add

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -2,7 +2,7 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const form = document.querySelector('.form');
 
-console.log(form.elements);
+console.log(form.elements[3]);
 
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
