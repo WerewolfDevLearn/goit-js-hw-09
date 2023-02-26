fe204b74df644b821d731d74f9cc0c6eeea63b2f
WerewolfDevLearn <werewolfdevlearn@gmail.com>
2023-02-27 01:35:42 +0200

    Task03 done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,14 +1,41 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const form = document.querySelector('.form');
+const amount = form.elements.amount.valueAsNumber;
+let delay = form.elements.delay.valueAsNumber;
+const step = form.elements.step.valueAsNumber;
 
-console.log(form.elements[3]);
+form.elements.submit.addEventListener('click', submitHandler);
 
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      const promisObj = { position: position, delay: delay };
+      if (shouldResolve) {
+        resolve(promisObj);
+      } else {
+        reject(promisObj);
+      }
+    }, delay);
+  });
+}
+
+function submitHandler(event) {
+  event.preventDefault();
+
+  for (let position = 1; position <= amount; position++) {
+    delay = delay + step;
+
+    console.log('step: ', step);
+    console.log('delay set: ', delay);
+
+    createPromise(position, delay)
+      .then(({ position, delay }) =>
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`)
+      )
+      .catch(({ position, delay }) =>
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`)
+      );
   }
 }
