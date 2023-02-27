227d082d62cdd9cbe86e18a39d31329f07ce56df
WerewolfDevLearn <werewolfdevlearn@gmail.com>
2023-02-27 15:22:37 +0200

    fix task 03

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,10 +1,6 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const form = document.querySelector('.form');
-const amount = form.elements.amount.valueAsNumber;
-let delay = form.elements.delay.valueAsNumber;
-const step = form.elements.step.valueAsNumber;
-
 form.elements.submit.addEventListener('click', submitHandler);
 
 function createPromise(position, delay) {
@@ -23,19 +19,19 @@ function createPromise(position, delay) {
 
 function submitHandler(event) {
   event.preventDefault();
+  const amount = form.elements.amount.valueAsNumber;
+  let delay = form.elements.delay.valueAsNumber;
+  const step = form.elements.step.valueAsNumber;
 
   for (let position = 1; position <= amount; position++) {
-    delay = delay + step;
-
-    console.log('step: ', step);
-    console.log('delay set: ', delay);
-
     createPromise(position, delay)
       .then(({ position, delay }) =>
         Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`)
       )
-      .catch(({ position, delay }) =>
-        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`)
-      );
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+
+    delay = delay + step;
   }
 }
