3f6b9bc777984d7bf4e3a572b29bbe8ce79644e8
WerewolfDevLearn <werewolfdevlearn@gmail.com>
2023-03-01 22:54:44 +0200

    add tast 04

--- src/js/04-timer-custom.js
+++ src/js/04-timer-custom.js
@@ -41,22 +41,71 @@ const fp = flatpickr('input#datetime-picker', options);
 refs.sartBtn.addEventListener('click', startCD);
 
 function startCD() {
+  refs.timeInput.setAttribute('disabled', '');
   refs.sartBtn.removeEventListener('click', startCD);
   refs.sartBtn.addEventListener('click', stopCD);
   refs.sartBtn.textContent = 'Stop';
   interval = setInterval(decreaseDelta, 1000);
+  // delta = selectedDateMls - currentDateMls;
 }
 
 function stopCD() {
   clearInterval(interval);
-  refs.sartBtn.textContent = 'Start';
   refs.sartBtn.removeEventListener('click', stopCD);
   refs.sartBtn.addEventListener('click', startCD);
+  refs.sartBtn.textContent = 'Start';
+  refs.sartBtn.classList.toggle('visually-hidden');
+  createBTN();
+  console.log(delta);
 }
 
 function decreaseDelta() {
   delta = delta - 1000;
   if (delta >= 0) {
     setTimer(refs, delta);
+    console.log(delta);
   }
+  if (delta === 0) {
+    refs.sartBtn.textContent = 'Start';
+    refs.sartBtn.setAttribute('disabled', '');
+    refs.timeInput.removeAttribute('disabled', '');
+  }
+}
+
+function createBTN() {
+  const resetBTN = document.createElement('button');
+  resetBTN.setAttribute('type', 'button');
+  resetBTN.setAttribute('data-reset', '');
+  resetBTN.textContent = 'Reset';
+  resetBTN.addEventListener('click', resetTimer);
+  const continueBTN = document.createElement('button');
+  continueBTN.setAttribute('type', 'button');
+  continueBTN.setAttribute('data-continue', '');
+  continueBTN.textContent = 'Continue';
+  continueBTN.addEventListener('click', continueTimer);
+  refs.timeInput.insertAdjacentElement('afterend', resetBTN);
+  refs.timeInput.insertAdjacentElement('afterend', continueBTN);
+}
+
+function resetTimer() {
+  delta = 0;
+  setTimer(refs, delta);
+  refs.timeInput.removeAttribute('disabled', '');
+  refs.sartBtn.classList.toggle('visually-hidden');
+  refs.sartBtn.setAttribute('disabled', '');
+  document.querySelector('[data-reset]').remove();
+  document.querySelector('[data-continue]').remove();
+}
+function continueTimer() {
+  const currentDate2 = new Date();
+  const currentDateMls2 = Date.parse(currentDate2);
+  delta = Date.parse(fp.selectedDates[0]) - currentDateMls2;
+  console.log(delta);
+  interval = setInterval(decreaseDelta, 1000);
+  document.querySelector('[data-reset]').remove();
+  document.querySelector('[data-continue]').remove();
+  refs.sartBtn.classList.toggle('visually-hidden');
+  refs.sartBtn.removeEventListener('click', startCD);
+  refs.sartBtn.addEventListener('click', stopCD);
+  refs.sartBtn.textContent = 'Stop';
 }
